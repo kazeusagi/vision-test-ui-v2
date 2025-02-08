c04b088fd1dc2a71ac13dda92016896229e9780c
kazeusagi <toshiki1098@gmail.com>
2025-02-08 23:55:14 +0000

    fix: パイプラインの調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { ToggleTheme } from '@/components/Mui/ToggleTheme';
-import { Paper } from '@mui/material';
+import { Paper, Typography } from '@mui/material';
 
 export default function Home() {
 	return (
 		<>
 			<Paper>Hello</Paper>
 			<ToggleTheme />
+			<Typography>1.0.0</Typography>
 		</>
 	);
 }
