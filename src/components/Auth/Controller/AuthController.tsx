9a9e2fa41e623655205658e6c02a9dc3ab373dc7
kazeusagi <toshiki1098@gmail.com>
2025-02-15 02:23:58 +0000

    add: ユーザー名の表示

--- src/components/Auth/Controller/AuthController.tsx
+++ src/components/Auth/Controller/AuthController.tsx
@@ -1,12 +1,17 @@
 'use client';
 
-import { Paper } from '@mui/material';
+import { sessionAtom } from '@/utils/atoms';
+import { Paper, Typography } from '@mui/material';
+import { useAtomValue } from 'jotai';
 import { SignInButton } from './SignInButton';
 import { SignOutButton } from './SignOutButton';
 
 export function AuthController() {
+	const session = useAtomValue(sessionAtom);
+
 	return (
 		<Paper sx={{ position: 'absolute', top: '0', right: '0' }}>
+			{session && <Typography>{`こんにちわ！${session.user?.name}さん！`}</Typography>}
 			<SignInButton />
 			<SignOutButton />
 		</Paper>
